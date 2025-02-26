d69c057d919bbaf04d617b64f7581a5c5b4d77f1
dhokabhoomi <bhoomidhoka@gmail.com>
2025-02-26 15:01:18 +0530

    Added functionality to Delete button

--- src/ToDoTask1.jsx
+++ src/ToDoTask1.jsx
@@ -1,11 +1,32 @@
-function ToDoTask1({ task, date }) {
+import React from "react";
+
+function ToDoTask1({ tasks, setTasks }) {
+  function deleteTask(index) {
+    let updatedList = tasks
+      .map((task, i) => (i === index ? null : task))
+      .filter((task) => task !== null);
+    setTasks(updatedList);
+  }
+  if (!tasks || !Array.isArray(tasks)) {
+    console.error("Tasks is not an Array: ", tasks);
+    return <div>No Tasks available</div>;
+  }
   return (
-    <div className="row mb-2">
-      <div className="col-sm-5">{task}</div>
-      <div className="col-sm-5">{date}</div>
-      <div className="col-sm-2 d-flex">
-        <button className="btn btn-danger">Delete</button>
-      </div>
+    <div className="Task-container">
+      {tasks.map((task, index) => (
+        <div key={index} className="row mb-2">
+          <div className="col-md-5">{task.text}</div>
+          <div className="col-md-5">{task.date}</div>
+          <div className="col-sm-2 d-flex">
+            <button
+              className="btn btn-danger"
+              onClick={() => deleteTask(index)}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
