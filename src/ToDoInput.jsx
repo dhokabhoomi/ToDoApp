f07c614285394643eef77b60f0fcc7f04c81a3a0
dhokabhoomi <bhoomidhoka@gmail.com>
2025-02-26 15:15:07 +0530

    Added complete functionality to Add Button

--- src/ToDoInput.jsx
+++ src/ToDoInput.jsx
@@ -1,25 +1,40 @@
-function ToDoInput() {
-  function addItem() {
-    let taskText = document.getElementById("todoInput").value;
-    let taskDate = document.getElementById("todoDate").value;
+import { useState } from "react";
 
-    if (taskText.trim() && taskDate) {
-      alert(`${taskText} added on ${taskDate}`);
+function ToDoInput({ setTasks, tasks }) {
+  let [taskText, settaskText] = useState("");
+  let [taskDate, settaskDate] = useState("");
+  function addItem() {
+    // let taskText = document.getElementById("todoInput").value;
+    // let taskDate = document.getElementById("todoDate").value;
+
+    if (taskText && taskDate) {
+      // alert(`${taskText} added on ${taskDate}`);
+      let task = [...tasks, { text: taskText, date: taskDate }];
+      console.log(task);
+      setTasks(task);
+      settaskText("");
+      settaskDate("");
     }
   }
 
   return (
-    <div className="row mb-2">
+    <div className="row mb-3">
       <div className="col-sm-5">
         <input
           type="text"
           className="form-control"
           placeholder="Task Name"
-          id="todoInput"
+          value={taskText}
+          onChange={(e) => settaskText(e.target.value)}
         />
       </div>
       <div className="col-sm-5">
-        <input type="date" className="form-control" id="todoDate" />
+        <input
+          type="date"
+          className="form-control"
+          value={taskDate}
+          onChange={(e) => settaskDate(e.target.value)}
+        />
       </div>
       <div className="col-sm-2 d-flex">
         <button className="btn btn-success" onClick={addItem}>
